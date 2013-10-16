99647ecc3e603a192e64fe77457d352a4fe0d249
Anthony Molinaro <anthony.molinaro@openx.com>
2013-10-16 14:28:52 -0700

    Allow for capitalized characters in paths tokens

--- test/ejsonpath_tests.erl
+++ test/ejsonpath_tests.erl
@@ -9,7 +9,6 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-
 all_test_() ->
     Doc = get_doc(),
     Pairs = [
@@ -86,9 +85,13 @@ all_test_() ->
                 end}]},
 
              {"Index-eval on array",
-             "$.store.book[(1)].author", [<<"Evelyn Waugh">>]},
+              "$.store.book[(1)].author", [<<"Evelyn Waugh">>]},
              {"Index-eval on hash",
-             "$.store.book[(1)][('author')]", [<<"Evelyn Waugh">>]}
+              "$.store.book[(1)][('author')]", [<<"Evelyn Waugh">>]},
+             {"Capitals should work in paths",
+              "$.store.LOLs.CHEEZBURG", [<<"no">>]},
+             {"Capitals should work in paths",
+              "$.store.LOLs.Cats", [<<"yes">>]}
 
             ],
     lists:map(
@@ -114,24 +117,28 @@ get_doc() ->
     %% io:format(user, "~p~n", [jiffy:decode(Bin)]),
     %% jiffy:decode(Bin)
     {[{<<"store">>,
-       {[{<<"book">>,
-          [{[{<<"category">>,<<"reference">>},
-             {<<"author">>,<<"Nigel Rees">>},
-             {<<"title">>,<<"Sayings of the Century">>},
-             {<<"price">>,8.95}]},
-           {[{<<"category">>,<<"fiction">>},
-             {<<"author">>,<<"Evelyn Waugh">>},
-             {<<"title">>,<<"Sword of Honour">>},
-             {<<"price">>,12.99}]},
-           {[{<<"category">>,<<"fiction">>},
-             {<<"author">>,<<"Herman Melville">>},
-             {<<"title">>,<<"Moby Dick">>},
-             {<<"isbn">>,<<"0-553-21311-3">>},
-             {<<"price">>,8.99}]},
-           {[{<<"category">>,<<"fiction">>},
-             {<<"author">>,<<"J. R. R. Tolkien">>},
-             {<<"title">>,<<"The Lord of the Rings">>},
-             {<<"isbn">>,<<"0-395-19395-8">>},
-             {<<"price">>,22.99}]}]},
-         {<<"bicycle">>,{[{<<"color">>,<<"red">>},
-                          {<<"price">>,19.95}]}}]}}]}.
+          {[{<<"book">>,
+                [{[{<<"category">>,<<"reference">>},
+                      {<<"author">>,<<"Nigel Rees">>},
+                      {<<"title">>,<<"Sayings of the Century">>},
+                      {<<"price">>,8.95}]},
+                  {[{<<"category">>,<<"fiction">>},
+                      {<<"author">>,<<"Evelyn Waugh">>},
+                      {<<"title">>,<<"Sword of Honour">>},
+                      {<<"price">>,12.99}]},
+                  {[{<<"category">>,<<"fiction">>},
+                      {<<"author">>,<<"Herman Melville">>},
+                      {<<"title">>,<<"Moby Dick">>},
+                      {<<"isbn">>,<<"0-553-21311-3">>},
+                      {<<"price">>,8.99}]},
+                  {[{<<"category">>,<<"fiction">>},
+                      {<<"author">>,<<"J. R. R. Tolkien">>},
+                      {<<"title">>,<<"The Lord of the Rings">>},
+                      {<<"isbn">>,<<"0-395-19395-8">>},
+                      {<<"price">>,22.99}]}]},
+              {<<"bicycle">>,
+                {[{<<"color">>,<<"red">>},{<<"price">>,19.95}]}},
+              {<<"LOLs">>,
+                {[{<<"Cats">>,<<"yes">>},
+                    {<<"Dogs">>,<<"yes">>},
+                    {<<"CHEEZBURG">>,<<"no">>}]}}]}}]}.
